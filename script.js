4f458416fb38d2dbef7528f682c3d364155e21a9
Michael Adebambo <80793630+Blazing-Mike@users.noreply.github.com>
2021-11-01 14:16:31 +0100

    updated cart functionality
    added cart functionality. Code is scrappy but it works

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const toggle = document.querySelector('.Navbar__toggle');
 const closeBtn = document.querySelector('.closebtn');
 const Navs = document.querySelector('.Navbar__items');
+const CartCount = document.querySelector('.cart-number');
+const cartIcon = document.querySelector('.cart-container')
+const cartNumberBox = document.querySelector('.notification');
+const cartModal = document.querySelector('.cart-modal');
+const addToCart = document.querySelector('.btn-submit');
+const NoOfProduct = document.querySelector('.no-of-items');
+const deleteIcon = document.querySelector('.delete-icon')
 
 toggle.addEventListener('click', function (e) {
     Navs.style.width = '50%';
@@ -65,9 +72,62 @@ const btnCount = document.querySelector('.count');
 btnPlus.addEventListener('click', function (e) {
     e.preventDefault();
     btnCount.textContent = count++;
+
 });
 
 btnMinus.addEventListener('click', function (e) {
 
     btnCount.textContent = count--;
+ 
 });
+
+cartIcon.addEventListener('click', (event) =>{
+    cartModal.classList.toggle('cart-modal-toggle');
+});
+
+
+
+addToCart.addEventListener('click', () =>{
+
+cartNumberBox.style.display = 'flex';
+cartNumberBox.style.alignItems = 'center';
+cartNumberBox.style.justifyContent = 'center';
+CartCount.textContent = btnCount.textContent;
+    document.querySelector('.empty-cart').style.display = 'none';
+    document.querySelector('.cart-item').style.display = 'flex';
+    document.querySelector('.cart-item').style.alignItems = 'center';
+    document.querySelector('.checkout').style.display = 'block';
+    NoOfProduct.textContent = btnCount.textContent;
+});
+
+
+deleteIcon.addEventListener('click', () =>{
+    document.querySelector('.cart-item').style.display = 'none';
+    document.querySelector('.checkout').style.display = 'none';
+    document.querySelector('.empty-cart').style.display = 'flex';
+    btnCount.textContent = 0;
+    cartNumberBox.style.display = 'none';
+    
+
+
+});
+
+
+/* addToCart.addEventListener('click', () =>{
+let markup = `
+<div class="flex-perfect gap-x cart-item">
+  <div class=" thumbnail-container flex-aligned">
+    <img src="./images/image-product-1-thumbnail.jpg" alt="image-product-1-thumbnail" class="modal-thumbnail">
+  </div>
+  <div class="thumbnail-desc">
+    <p class="thumbnail-para">Autumn Limited Edition</p>
+    <p class="thumnnail-para">$125.00 * <span class="no-of-items"></span> <span class="bold-text">$375.00</span> </p>
+  </div>
+
+  <div class="delete-btn">
+    <img src="./images/icon-delete.svg" alt="icon-delete" class="delete-icon">
+  </div>
+</div>` ;
+cartModal.innerHTML = markup;
+NoOfProduct.innerHTML = btnCount.textContent;
+}) */
